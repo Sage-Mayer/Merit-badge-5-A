46dcc76ecb5403a6ed780065f629f40d1a5eeedd
Sage-Mayer <62776748+Sage-Mayer@users.noreply.github.com>
2020-03-28 10:09:18 -0600

    Add files via upload

---
+++ App.py
@@ -0,0 +1,6 @@
+character_age = "40"
+character_name = "Tom"
+print ("there was a man named " + character_name + ", " )
+print("he was " + character_age + "," )
+print("he liked his name " + character_name + "," )
+print("but didn't like that he was " + character_age + ",")
